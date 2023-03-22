e9cdc3d54f9028b50eadc4bf28965e97ac89304a
OscarCodev <OscarCodev@users.noreply.github.com>
2023-03-22 10:39:25 -0500

    Agregar logica para avanzar o retroceder la pregunta

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,29 @@ function App() {
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const question = questions[currentQuestion] // preguntas contiene a las preguntas en la posicion 0
+  
+  const clickedAnswer = (isRight) => {
+    setCurrentQuestion(isRight ?
+      currentQuestion < questions.length - 1 ? currentQuestion + 1: 0:
+      currentQuestion > 0 ? currentQuestion - 1: 0  
+    )
+  }
+  
   return (
     <>
       <h1>Trivial</h1>
       <p>{question.question}</p>
       <div>
-        {
-          question.answers.map(obj => <p><button>X</button>{" " + obj.txt}</p>)
+        {question.answers.map(obj => (
+            <p>
+              <button
+                onClick={()=> clickedAnswer(obj.isRight)}
+              >
+              X
+              </button>
+              {" " + obj.txt}
+            </p>
+        ))
         }
       </div>
     </>
